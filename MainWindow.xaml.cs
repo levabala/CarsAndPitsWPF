840c85308b71ccf9a3b2ebfb7479aefa0a34c6f6
levabala <levabala@ya.ru>
2017-06-29 14:54:49 +0300

    v0.1
    It works!

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarsAndPitsWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ValuesNet net = new ValuesNet(23);
+        int currentLevel = 3;
+        int valuesCount = 1000000;              
+         
+        public MainWindow()
+        {
+            InitializeComponent();            
+            init();
+
+            MyWindow.Loaded += MyWindow_Loaded;
+        }
+
+        private void MyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            KeyDown += MainWindow_KeyDown;
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Add:
+                    if (currentLevel >= net.maxDepth)
+                        break;
+                    currentLevel++;                    
+                    if (!Keyboard.IsKeyDown(Key.LeftShift))
+                        MainCanvas.Children.Clear();
+                    addRectsToCanvas(net.generateRectangelsLayer(currentLevel));                    
+                    break;
+                case Key.Subtract:
+                    if (currentLevel < 2)
+                        break;
+                    currentLevel--;                    
+                    if (!Keyboard.IsKeyDown(Key.LeftShift))
+                        MainCanvas.Children.Clear();
+                    addRectsToCanvas(net.generateRectangelsLayer(currentLevel));                    
+                    break;
+                case Key.C:
+                    MainCanvas.Children.Clear();
+                    break;
+                case Key.Escape:
+                    Application.Current.Shutdown();
+                    break;
+            }
+        }
+
+        public void init()
+        {
+            Random rnd = new Random();                        
+            for (int i = 0; i < valuesCount; i++)
+            {
+                Point p = new Point(rnd.NextDouble() * 360 - 180, rnd.NextDouble() * 180 - 90);
+                net.putValue(p.Y, p.X, 3);// rnd.NextDouble() * 10 - 5);
+
+                /*Ellipse circle = new Ellipse();                
+                circle.Width = 3;
+                circle.Height = 3;
+                circle.Opacity = 1;
+                circle.Fill = Brushes.Green;
+                Canvas.SetLeft(circle, points[i].X + 180 - 1.5);
+                Canvas.SetTop(circle, points[i].Y + 90 - 1.5);
+                MainCanvas.Children.Add(circle);*/
+            }
+
+            List<Rectangle> rects = new List<Rectangle>();
+            //rects.AddRange(net.generateChildRectangles(net.baseSquare));            
+            rects.AddRange(net.generateRectangelsLayer(3));
+
+            addRectsToCanvas(rects);                        
+
+            double ScaleRateX = MyWindow.Width / 180;
+            double ScaleRateY = MyWindow.Height / 180;
+            double ScaleRate = 4;// Math.Min(ScaleRateX, ScaleRateY);
+            ScaleTransform scale = new ScaleTransform(
+                MainCanvas.LayoutTransform.Value.M11 * ScaleRate, MainCanvas.LayoutTransform.Value.M22 * ScaleRate
+                );
+            MainCanvas.LayoutTransform = scale;
+            MainCanvas.UpdateLayout();
+        }
+
+        private void addRectsToCanvas(List<Rectangle> rects)
+        {
+            if (currentLevel < 8)
+                foreach (Rectangle rect in rects)
+                    MainCanvas.Children.Add(rect);
+
+            Title = string.Format("Total values: {0}, Total rectangles: {1}, Level: {2}, Level rectangles: {3}",
+                valuesCount.ToString(), net.totalSquaresCount.ToString(), currentLevel.ToString(), rects.Count.ToString());
+        }
+    }
+}
---
+++ ValuesNet.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarsAndPitsWPF
+{
+    class ValuesNet
+    {
+        public Square baseSquare = new Square(-90, -180, 0, 0);
+        public readonly int maxDepth;
+        public double maxValue = 200;
+        public int totalSquaresCount = 0;        
+
+        public ValuesNet(int maxDepth = 5)
+        {
+            this.maxDepth = maxDepth;
+        }
+
+        public void putValue(double lat, double lng, double value)
+        {            
+            putToSquareTree(lat, lng, value);
+        }
+
+        public List<Rectangle> generateChildRectangles(Square square)
+        {            
+            List<Rectangle>[] rectOfRects = new List<Rectangle>[] {
+                    new List<Rectangle>(),new List<Rectangle>(),new List<Rectangle>(),new List<Rectangle>()
+                };
+            for (int i = 0; i < 4; i++)
+                if (square.children[i] != null)
+                {
+                    rectOfRects[i].Add(generateWPFRectange(square.children[i]));
+                    rectOfRects[i].AddRange(generateChildRectangles(square.children[i]));
+                }
+
+            List<Rectangle> rects = new List<Rectangle>();
+            bool nextAvailable = (rectOfRects[0].Count + rectOfRects[1].Count + rectOfRects[2].Count + rectOfRects[3].Count) > 0;
+            int index = 0;
+            while (nextAvailable) {
+                nextAvailable = false;              
+                if (index < rectOfRects[0].Count)
+                {
+                    rects.Add(rectOfRects[0][index]);
+                    nextAvailable = true;
+                }
+                if (index < rectOfRects[1].Count)
+                {
+                    rects.Add(rectOfRects[1][index]);
+                    nextAvailable = true;
+                }
+                if (index < rectOfRects[2].Count)
+                {
+                    rects.Add(rectOfRects[2][index]);
+                    nextAvailable = true;
+                }
+                if (index < rectOfRects[3].Count)
+                {
+                    rects.Add(rectOfRects[3][index]);
+                    nextAvailable = true;
+                }
+                index++;
+            }
+
+            return rects;
+        }
+
+        public Rectangle generateWPFRectange(Square square)
+        {
+            maxValue = baseSquare.value * 1.001;
+
+            Rectangle rect = new Rectangle();
+            rect.Stroke = Brushes.Black;
+            rect.StrokeThickness = 0.25;
+            double intesity = 255 - ((square.value != 0 && square.level != 0) ? square.value : 1) / maxValue * 255;
+            Color color = Color.FromRgb(255, (byte)intesity, (byte)intesity);
+            Brush fillBrush = new SolidColorBrush(color);
+            fillBrush.Opacity = 0.5;
+            rect.Fill = fillBrush;                        
+            rect.Width = 360 / Math.Pow(2, square.level);
+            rect.Height = 180 / Math.Pow(2, square.level); //we use 180 instead 360 to get squares (not rectangles)
+            double left = square.lng + 180;
+            double top = square.lat + 90;
+            Canvas.SetLeft(rect, left);
+            Canvas.SetTop(rect, top);                        
+            return rect;
+        }
+
+        public List<Rectangle> generateRectangelsLayer(int level)
+        {
+            List<Rectangle> layer = new List<Rectangle>();
+            foreach (Square s in getLayer(baseSquare, level))
+                layer.Add(generateWPFRectange(s));
+            return layer;
+        }
+
+        public List<Square> getLayer(Square startSquare, int level)
+        {
+            List<Square> layer = new List<Square>();
+            for (int i = 0; i < startSquare.children.Length; i++)
+                if (startSquare.children[i] == null) continue;
+                else if (startSquare.children[i].level == level)
+                    layer.Add(startSquare.children[i]);
+                else layer.AddRange(getLayer(startSquare.children[i], level));
+
+            return layer;
+        }
+
+        public void putToSquareTree(double lat, double lng, double value)
+        {
+            Square square = baseSquare;
+            baseSquare.value += value;
+
+            int[] path = getPathToSquare(lat, lng);
+            foreach (int i in path)
+            {
+                if (square.children[i] == null)
+                {
+                    square.children[i] = new Square(square, i, 0);
+                    totalSquaresCount++;
+                }
+
+                square = square.children[i];
+                square.value += value;
+            }
+        }
+        
+        public Square getSquare(double lat, double lng)
+        {            
+            Square square = baseSquare;
+
+            int[] path = getPathToSquare(lat, lng);
+            square = getSquare(path);
+
+            return square;
+        }
+
+        public Square getSquare(int[] path)
+        {
+            Square square = baseSquare;            
+            foreach (int i in path)
+            {
+                if (square.children == null)
+                {
+                    square.children = new Square[4];
+                    square.children[i] = new Square(square, i, 0);
+                    totalSquaresCount++;
+                }
+                else if (square.children[i].children == null)
+                {
+                    square.children[i] = new Square(square, i, 0);
+                    totalSquaresCount++;
+                }
+
+                square = square.children[i];                
+            }                
+
+            return square;
+        }
+
+        public int[] getPathToSquare(double lat, double lng)
+        {
+            int[] path = new int[maxDepth-1];            
+            double squareLat = -90;
+            double squareLng = -180;
+            for (int level = 0; level < maxDepth-1; level++)
+            {                
+                double sHeight = 180 / Math.Pow(2, level);
+                double sWidth = 360 / Math.Pow(2, level); //squareWidth
+                double centerLat = squareLat + sHeight / 2;
+                double centerLng = squareLng + sWidth / 2;
+
+                //can I replace it??
+                int index = 0;
+                if (lat > centerLat)
+                {
+                    if (lng > centerLng)
+                    {
+                        index = 3;
+                        squareLng = centerLng;
+                    }
+                    else index = 2;                                            
+                    squareLat = centerLat;
+                }
+                else if (lng > centerLng)
+                {
+                    index = 1;
+                    squareLng = centerLng;
+                }                
+
+                path[level] = index;
+            }
+
+            return path;
+        }     
+    }
+
+    class Square
+    {
+        public int level;
+        public double lat;
+        public double lng ;
+        public double value;
+        public Square[] children;                
+
+        public Square(double lat, double lng, int level, double value)
+        {
+            this.level = level;
+            this.lat = lat;
+            this.lng = lng;
+            this.value = value;
+            children = new Square[4];            
+        }
+
+        public Square(Square parent, int index, double value)
+        {
+            level = parent.level + 1;
+            lat = parent.lat + ((index > 1) ? 180 / Math.Pow(2, parent.level) / 2 : 0);
+            lng = parent.lng + ((index == 1 || index == 3) ? 360 / Math.Pow(2, parent.level) / 2 : 0);
+            children = new Square[4];
+            this.value = value;
+        }
+    }
+}
