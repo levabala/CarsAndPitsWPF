69bffcf39b96fb6c99ba000a48d365c0c9d887d6
levabala <levabala@ya.ru>
2017-07-01 18:04:23 +0300

    v0.3.2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace CarsAndPitsWPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
         ValuesNet net;
         int currentLevel = 3;
